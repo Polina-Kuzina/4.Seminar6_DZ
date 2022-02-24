79c9549ce9400767a64cb18b1b6cd68a9780fc49
Polina <payline@mail.ru>
2022-02-24 08:27:54 +0300

    46

--- 46/Program.cs
+++ 46/Program.cs
@@ -14,34 +14,3 @@ for(int i = 0; i<str.Length; i=i+2)
 {
     Console.Write($"({str[i]*k},{str[i+1]*k}) ");
 }
-
-
-
-
-
-
-//Console.Write("Задайте коэффициент масштабирования k = ");
-//int k = int.Parse(Console.ReadLine());
-//Console.WriteLine("Исходные координаты:");
-//string text = "(1,2) (2,6) (3,9) (3,8)"
-    //  .Replace("(","")
-     // .Replace(")","")
-    //  ;
-//Console.WriteLine(text);
-//var data = text.Split(" ")
-      //  .Select(item => item.Split(','))
-     //  .ToArray();
-//Console.WriteLine("Координаты, увеличенные на " + k);
-//for(int i = 0; i<data.Length; i++)
-//{
-  //  int x = int.Parse(data[i]);
-   // x = x * k;
-  //  for(int j = 0; j<data[i].Length; j++)
-   // {
-     //   int y = int.Parse(data[i][j]);
-      //  y = y * k;
-      //  Console.Write($"({x},{y}) ");
-   // }
-    // Console.Write(" ");
-//}
-// string[] str = Console.ReadLine().Split(' ');
